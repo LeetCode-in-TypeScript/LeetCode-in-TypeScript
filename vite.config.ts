9e8f96c663502f4fa821cd5f47ea085b4b818868
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-10 14:45:56 +0200

    Added task 1

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+// vite.config.ts
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    include: ['**/*.{test,spec}.{ts,js}'],
+    reporters: ['verbose'],
+    maxThreads: 1,
+    minThreads: 1,
+    coverage: {
+      provider: 'istanbul',
+      include: ['src/**/*.ts'],
+      reporter: ['text', 'json', 'html'],
+    },
+  },
+});
