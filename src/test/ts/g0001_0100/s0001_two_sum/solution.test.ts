c921a504f247fe28bca8c63810ca7d5e7a4f2d5f
Valentyn Kolesnikov <javadev75@gmail.com>
2023-03-13 08:47:07 +0200

    Improved prettier

--- src/test/ts/g0001_0100/s0001_two_sum/solution.test.ts
+++ src/test/ts/g0001_0100/s0001_two_sum/solution.test.ts
@@ -1,19 +1,19 @@
 // tslint:disable:no-magic-numbers
-import { twoSum } from 'src/main/ts/g0001_0100/s0001_two_sum/solution';
-import { expect, test } from 'vitest';
+import { twoSum } from 'src/main/ts/g0001_0100/s0001_two_sum/solution'
+import { expect, test } from 'vitest'
 
 test('twoSum', () => {
-  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1]);
-});
+    expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
+})
 
 test('twoSum2', () => {
-  expect(twoSum([3, 2, 4], 6)).toEqual([1, 2]);
-});
+    expect(twoSum([3, 2, 4], 6)).toEqual([1, 2])
+})
 
 test('twoSum3', () => {
-  expect(twoSum([3, 3], 6)).toEqual([0, 1]);
-});
+    expect(twoSum([3, 3], 6)).toEqual([0, 1])
+})
 
 test('twoSum4', () => {
-  expect(twoSum([3, 3], 7)).toEqual(undefined);
-});
+    expect(twoSum([3, 3], 7)).toEqual(undefined)
+})
