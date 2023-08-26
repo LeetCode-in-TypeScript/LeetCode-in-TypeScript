3d286de22317e55a120761f4408605b2b8b92388
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-26 05:47:35 +0300

    Update solution.ts

--- src/main/ts/g0001_0100/s0003_longest_substring_without_repeating_characters/solution.ts
+++ src/main/ts/g0001_0100/s0003_longest_substring_without_repeating_characters/solution.ts
@@ -1,13 +1,12 @@
 // #Medium #Top_100_Liked_Questions #Top_Interview_Questions #String #Hash_Table #Sliding_Window
 // #Algorithm_I_Day_6_Sliding_Window #Level_2_Day_14_Sliding_Window/Two_Pointer #Udemy_Strings
-// #2023_03_16_Time_95_ms_(70.71%)_Space_49.1_MB_(49.42%)
+// #2023_08_26_Time_81_ms_(68.46%)_Space_49.4_MB_(32.21%)
 
 function lengthOfLongestSubstring(s: string): number {
     const hash: { [key: string]: number } = {}
     let maxLength = 0
     let length = 0
     let start = 0
-
     for (let i = 0; i < s.length; i++) {
         const char = s[i]
         if (hash[char] !== undefined && hash[char] >= start) {
