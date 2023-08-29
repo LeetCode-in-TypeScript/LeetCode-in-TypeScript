4ae8352a5c58415d519e483b1b2646b7ba1f882e
Valentyn Kolesnikov <javadev75@gmail.com>
2023-08-29 20:56:25 +0300

    Added tasks 21-25

---
+++ src/main/ts/g0001_0100/s0023_merge_k_sorted_lists/solution.ts
@@ -0,0 +1,70 @@
+// #Hard #Top_100_Liked_Questions #Top_Interview_Questions #Heap_Priority_Queue #Linked_List
+// #Divide_and_Conquer #Merge_Sort #2023_08_29_Time_75_ms_(97.06%)_Space_47.3_MB_(98.42%)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+    if (lists.length === 0) {
+        return null
+    }
+    return mergeKListsRecursive(lists, 0, lists.length)
+}
+
+function mergeKListsRecursive(lists: Array<ListNode | null>, leftIndex: number, rightIndex: number): ListNode | null {
+    if (rightIndex > leftIndex + 1) {
+        const mid = Math.floor((leftIndex + rightIndex) / 2)
+        const left = mergeKListsRecursive(lists, leftIndex, mid)
+        const right = mergeKListsRecursive(lists, mid, rightIndex)
+        return mergeTwoLists(left, right)
+    } else {
+        return lists[leftIndex]
+    }
+}
+
+function mergeTwoLists(left: ListNode | null, right: ListNode | null): ListNode | null {
+    if (left === null) {
+        return right
+    }
+    if (right === null) {
+        return left
+    }
+    let res: ListNode | null
+    if (left.val <= right.val) {
+        res = left
+        left = left.next
+    } else {
+        res = right
+        right = right.next
+    }
+    let node = res
+    while (left !== null || right !== null) {
+        if (left === null) {
+            node.next = right
+            right = right!.next
+        } else if (right === null) {
+            node.next = left
+            left = left.next
+        } else {
+            if (left.val <= right.val) {
+                node.next = left
+                left = left.next
+            } else {
+                node.next = right
+                right = right.next
+            }
+        }
+        node = node.next
+    }
+    return res
+}
+
+export { mergeKLists }
---
+++ src/main/ts/g0001_0100/s0024_swap_nodes_in_pairs/solution.ts
@@ -0,0 +1,52 @@
+// #Medium #Top_100_Liked_Questions #Linked_List #Recursion #Data_Structure_II_Day_12_Linked_List
+// #Udemy_Linked_List #2023_08_29_Time_59_ms_(62.67%)_Space_44.7_MB_(26.03%)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+function swapPairs(head: ListNode | null): ListNode | null {
+    if (head === null) {
+        return null
+    }
+    const len = getLength(head)
+    return reverse(head, len)
+}
+
+function getLength(curr: ListNode | null): number {
+    let cnt = 0
+    while (curr !== null) {
+        cnt++
+        curr = curr.next
+    }
+    return cnt
+}
+
+// Recursive function to reverse in groups
+function reverse(head: ListNode | null, len: number): ListNode | null {
+    // base case
+    if (len < 2) {
+        return head
+    }
+    let curr: ListNode | null = head
+    let prev: ListNode | null = null
+    let next: ListNode | null
+    for (let i = 0; i < 2; i++) {
+        // reverse linked list code
+        next = curr!.next
+        curr!.next = prev
+        prev = curr
+        curr = next
+    }
+    head!.next = reverse(curr, len - 2)
+    return prev
+}
+
+export { swapPairs }
