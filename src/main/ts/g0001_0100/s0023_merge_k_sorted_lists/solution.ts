fe9f8bf3f36c90ed3f72b7e9067a38faab41d884
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-24 09:58:56 +0300

    Fixed sonar

--- src/main/ts/g0001_0100/s0023_merge_k_sorted_lists/solution.ts
+++ src/main/ts/g0001_0100/s0023_merge_k_sorted_lists/solution.ts
@@ -47,10 +47,7 @@ function mergeTwoLists(left: ListNode | null, right: ListNode | null): ListNode 
     }
     let node = res
     while (left !== null || right !== null) {
-        if (left === null) {
-            node.next = right
-            right = right.next
-        } else if (right === null || left.val <= right.val) {
+        if (right === null || left.val <= right.val) {
             node.next = left
             left = left.next
         } else {
