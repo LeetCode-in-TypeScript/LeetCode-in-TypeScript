33379b7a821636dddf99541ed7876efa1d07a7e2
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-25 18:10:04 +0300

    Fixed vite

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    include: ['src/test/**/*.{test,spec}.{ts,js}'],
     reporters: ['verbose'],
     maxThreads: 5,
     minThreads: 1,
     coverage: {
       provider: 'istanbul',
-      include: ['src/**/*.ts'],
+      include: ['src/main/**/*.ts'],
       reporter: ['text', 'lcov'],
     },
   },
