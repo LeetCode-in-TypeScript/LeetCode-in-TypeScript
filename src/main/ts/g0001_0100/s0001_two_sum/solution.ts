87a85a8f44372542a9d37e66b49dc2a529f9d418
Valentyn Kolesnikov <javadev75@gmail.com>
2023-09-28 08:33:30 +0300

    Fixed task 1

--- src/main/ts/g0001_0100/s0001_two_sum/solution.ts
+++ src/main/ts/g0001_0100/s0001_two_sum/solution.ts
@@ -2,14 +2,14 @@
 // #Data_Structure_I_Day_2_Array #Level_1_Day_13_Hashmap #Udemy_Arrays #Big_O_Time_O(n)_Space_O(n)
 // #2023_09_28_Time_54_ms_(91.72%)_Space_45.5_MB_(22.26%)
 
-function twoSum(numbers: number[], target: number): number[] {
+function twoSum(nums: number[], target: number): number[] {
     const indexMap: Map<number, number> = new Map()
-    for (let i = 0; i < numbers.length; i++) {
-        const requiredNum: number = target - numbers[i]
+    for (let i = 0; i < nums.length; i++) {
+        const requiredNum: number = target - nums[i]
         if (indexMap.has(requiredNum)) {
             return [indexMap.get(requiredNum)!, i]
         }
-        indexMap.set(numbers[i], i)
+        indexMap.set(nums[i], i)
     }
     return [-1, -1]
 }
