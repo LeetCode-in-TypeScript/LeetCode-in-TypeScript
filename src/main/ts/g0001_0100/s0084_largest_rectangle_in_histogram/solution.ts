36cc8b1bbd3b07b1633b99998d30589a8a3d3f80
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-10-03 21:51:41 +0700

    Update solution.ts

--- src/main/ts/g0001_0100/s0084_largest_rectangle_in_histogram/solution.ts
+++ src/main/ts/g0001_0100/s0084_largest_rectangle_in_histogram/solution.ts
@@ -2,10 +2,10 @@
 // #Big_O_Time_O(n_log_n)_Space_O(log_n) #2023_10_03_Time_78_ms_(92.44%)_Space_56.8_MB_(85.29%)
 
 function largestRectangleArea(heights: number[]): number {
-    var stack = []
-    var nextSmall = new Array(heights.length).fill(heights.length)
-    var prevSmall = new Array(heights.length).fill(-1)
-    for (var i = 0; i < heights.length; i++) {
+    let stack = []
+    let nextSmall = new Array(heights.length).fill(heights.length)
+    let prevSmall = new Array(heights.length).fill(-1)
+    for (let i = 0; i < heights.length; i++) {
         while (stack.length !== 0 && heights[stack[stack.length - 1]] > heights[i]) {
             nextSmall[stack[stack.length - 1]] = i
             stack.pop()
@@ -16,9 +16,9 @@ function largestRectangleArea(heights: number[]): number {
         }
         stack.push(i)
     }
-    var maxArea = 0
-    for (var i = 0; i < heights.length; i++) {
-        var currentArea = heights[i] * (nextSmall[i] - prevSmall[i] - 1)
+    let maxArea = 0
+    for (let i = 0; i < heights.length; i++) {
+        let currentArea = heights[i] * (nextSmall[i] - prevSmall[i] - 1)
         maxArea = Math.max(maxArea, currentArea)
     }
 
