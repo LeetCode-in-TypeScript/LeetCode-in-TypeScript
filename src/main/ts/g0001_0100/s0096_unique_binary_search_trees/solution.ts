4848914cd1b5bba8d33c3faeb71c69adee6e999c
Valentyn Kolesnikov <javadev75@gmail.com>
2023-10-04 01:04:10 +0300

    Update solution.ts

--- src/main/ts/g0001_0100/s0096_unique_binary_search_trees/solution.ts
+++ src/main/ts/g0001_0100/s0096_unique_binary_search_trees/solution.ts
@@ -6,7 +6,11 @@ function numTrees(n: number): number {
     const uniqueCount = new Array(n + 1).fill(0)
     uniqueCount[0] = 1
     uniqueCount[1] = 1
-    for (let i = 2; i <= n; ++i) for (let j = 1; j <= i; ++j) uniqueCount[i] += uniqueCount[j - 1] * uniqueCount[i - j]
+    for (let i = 2; i <= n; ++i) {
+        for (let j = 1; j <= i; ++j) {
+            uniqueCount[i] += uniqueCount[j - 1] * uniqueCount[i - j]
+        }
+    }
     return uniqueCount[n]
 }
 
