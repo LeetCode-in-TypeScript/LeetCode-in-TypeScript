800e77f99020cfc747d6f9a421d54aa9aff6c586
ThanhNIT <thanhnguyen.it.work@gmail.com>
2023-10-03 21:58:28 +0700

    Update solution.test.ts

--- src/test/ts/g0001_0100/s0079_word_search/solution.test.ts
+++ src/test/ts/g0001_0100/s0079_word_search/solution.test.ts
@@ -2,41 +2,20 @@
 import { exist } from 'src/main/ts/g0001_0100/s0079_word_search/solution'
 import { expect, test } from 'vitest'
 
+const matrix = [
+    ['A', 'B', 'C', 'E'],
+    ['S', 'F', 'C', 'S'],
+    ['A', 'D', 'E', 'E'],
+]
+
 test('exist', () => {
-    expect(
-        exist(
-            [
-                ['A', 'B', 'C', 'E'],
-                ['S', 'F', 'C', 'S'],
-                ['A', 'D', 'E', 'E'],
-            ],
-            'ABCCED',
-        ),
-    ).toEqual(true)
+    expect(exist(matrix, 'ABCCED')).toEqual(true)
 })
 
 test('exist2', () => {
-    expect(
-        exist(
-            [
-                ['A', 'B', 'C', 'E'],
-                ['S', 'F', 'C', 'S'],
-                ['A', 'D', 'E', 'E'],
-            ],
-            'SEE',
-        ),
-    ).toEqual(true)
+    expect(exist(matrix, 'SEE')).toEqual(true)
 })
 
 test('exist3', () => {
-    expect(
-        exist(
-            [
-                ['A', 'B', 'C', 'E'],
-                ['S', 'F', 'C', 'S'],
-                ['A', 'D', 'E', 'E'],
-            ],
-            'ABCB',
-        ),
-    ).toEqual(false)
+    expect(exist(matrix, 'ABCB')).toEqual(false)
 })
