44e5934b82e62cda8507a7789f9e250f177f4668
Valentyn Kolesnikov <javadev75@gmail.com>
2023-10-06 18:34:28 +0300

    Update node.ts

--- src/main/ts/com_github_leetcode/node.ts
+++ src/main/ts/com_github_leetcode/node.ts
@@ -11,29 +11,23 @@ class Node {
     toString(): string {
         const result: string[] = []
         let curr: Node | null = this
-
         while (curr !== null) {
             const result2: string[] = []
             result2.push(String(curr.val))
-
             if (curr.random === null) {
                 result2.push('null')
             } else {
                 let randomIndex = 0
                 let curr2: Node | null = this
-
                 while (curr2?.next !== null && curr2 !== curr.random) {
                     randomIndex++
                     curr2 = curr2.next
                 }
-
                 result2.push(String(randomIndex))
             }
-
             result.push(`[${result2.join(',')}]`)
             curr = curr.next
         }
-
         return `[${result.join(',')}]`
     }
 }
