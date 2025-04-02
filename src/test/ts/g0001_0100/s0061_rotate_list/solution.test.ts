6414fe39661bcd907fc8bb635abef5cb2a4a9361
Valentyn Kolesnikov <javadev75@gmail.com>
2025-04-02 19:05:46 +0300

    Improved tasks 57-66

--- src/test/ts/g0001_0100/s0061_rotate_list/solution.test.ts
+++ src/test/ts/g0001_0100/s0061_rotate_list/solution.test.ts
@@ -4,9 +4,9 @@ import { expect, test } from 'vitest'
 import { constructLinkedList } from '../../com_github_leetcode/linkedlistutils'
 
 test('rotateRight', () => {
-    expect(rotateRight(constructLinkedList([1,2,3,4,5]), 2).toString()).toEqual('4, 5, 1, 2, 3')
+    expect(rotateRight(constructLinkedList([1, 2, 3, 4, 5]), 2).toString()).toEqual('4, 5, 1, 2, 3')
 })
 
 test('rotateRight2', () => {
-    expect(rotateRight(constructLinkedList([0,1,2]), 4).toString()).toEqual('2, 0, 1')
+    expect(rotateRight(constructLinkedList([0, 1, 2]), 4).toString()).toEqual('2, 0, 1')
 })
