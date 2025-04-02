f6a575f75489fb8353f24c38299997bfa9878bf8
Valentyn Kolesnikov <javadev75@gmail.com>
2025-04-02 18:34:05 +0300

    Update solution.test.ts

--- src/test/ts/g0001_0100/s0057_insert_interval/solution.test.ts
+++ src/test/ts/g0001_0100/s0057_insert_interval/solution.test.ts
@@ -6,6 +6,6 @@ test('insert', () => {
     expect(insert([[1,3],[6,9]], [2,5])).toEqual([[1,5],[6,9]])
 })
 
-test('insert', () => {
+test('insert2', () => {
     expect(insert( [[1,2],[3,5],[6,7],[8,10],[12,16]], [4,8])).toEqual([[1,2],[3,10],[12,16]])
 })
