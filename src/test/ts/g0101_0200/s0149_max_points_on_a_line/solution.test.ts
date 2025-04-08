584c2f12277ee7c5cf12b96838e79fb2c103b3c3
Valentyn Kolesnikov <javadev75@gmail.com>
2025-04-09 01:47:33 +0300

    Improved tasks 149, 162

--- src/test/ts/g0101_0200/s0149_max_points_on_a_line/solution.test.ts
+++ src/test/ts/g0101_0200/s0149_max_points_on_a_line/solution.test.ts
@@ -3,9 +3,24 @@ import { maxPoints } from 'src/main/ts/g0101_0200/s0149_max_points_on_a_line/sol
 import { expect, test } from 'vitest'
 
 test('maxPoints', () => {
-    expect(maxPoints([[1,1],[2,2],[3,3]])).toEqual(3)
+    expect(
+        maxPoints([
+            [1, 1],
+            [2, 2],
+            [3, 3],
+        ]),
+    ).toEqual(3)
 })
 
 test('maxPoints2', () => {
-    expect(maxPoints([[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]])).toEqual(4)
+    expect(
+        maxPoints([
+            [1, 1],
+            [3, 2],
+            [5, 3],
+            [4, 1],
+            [2, 3],
+            [1, 4],
+        ]),
+    ).toEqual(4)
 })
--- src/test/ts/g0101_0200/s0162_find_peak_element/solution.test.ts
+++ src/test/ts/g0101_0200/s0162_find_peak_element/solution.test.ts
@@ -3,9 +3,9 @@ import { findPeakElement } from 'src/main/ts/g0101_0200/s0162_find_peak_element/
 import { expect, test } from 'vitest'
 
 test('findPeakElement', () => {
-    expect(findPeakElement([1,2,3,1])).toEqual(2)
+    expect(findPeakElement([1, 2, 3, 1])).toEqual(2)
 })
 
 test('findPeakElement2', () => {
-    expect(findPeakElement([1,2,1,3,5,6,4])).toEqual(5)
+    expect(findPeakElement([1, 2, 1, 3, 5, 6, 4])).toEqual(5)
 })
