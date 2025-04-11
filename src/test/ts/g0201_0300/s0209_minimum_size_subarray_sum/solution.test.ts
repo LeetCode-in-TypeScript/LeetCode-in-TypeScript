d10a54bf506cd9ff92069c607a6ca126e21e182e
Valentyn Kolesnikov <javadev75@gmail.com>
2025-04-11 05:06:46 +0300

    Improved tasks 173, 209

--- src/test/ts/g0201_0300/s0209_minimum_size_subarray_sum/solution.test.ts
+++ src/test/ts/g0201_0300/s0209_minimum_size_subarray_sum/solution.test.ts
@@ -3,13 +3,13 @@ import { minSubArrayLen } from 'src/main/ts/g0201_0300/s0209_minimum_size_subarr
 import { expect, test } from 'vitest'
 
 test('minSubArrayLen', () => {
-    expect(minSubArrayLen(7, [2,3,1,2,4,3])).toEqual(2)
+    expect(minSubArrayLen(7, [2, 3, 1, 2, 4, 3])).toEqual(2)
 })
 
 test('minSubArrayLen2', () => {
-    expect(minSubArrayLen(4, [1,4,4])).toEqual(1)
+    expect(minSubArrayLen(4, [1, 4, 4])).toEqual(1)
 })
 
 test('minSubArrayLen3', () => {
-    expect(minSubArrayLen(11, [1,1,1,1,1,1,1,1])).toEqual(0)
+    expect(minSubArrayLen(11, [1, 1, 1, 1, 1, 1, 1, 1])).toEqual(0)
 })
