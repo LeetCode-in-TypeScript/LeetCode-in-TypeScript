7793a6db73f2cb5ff9991e2a2590ab0e13ff766e
Valentyn Kolesnikov <javadev75@gmail.com>
2025-04-11 05:03:34 +0300

    Improved tasks 173, 209

--- src/main/ts/g0101_0200/s0173_binary_search_tree_iterator/solution.ts
+++ src/main/ts/g0101_0200/s0173_binary_search_tree_iterator/solution.ts
@@ -2,7 +2,7 @@
 // #Data_Structure_II_Day_17_Tree #Programming_Skills_II_Day_16 #Level_2_Day_9_Binary_Search_Tree
 // #Top_Interview_150_Binary_Tree_General #2025_04_09_Time_6_ms_(98.16%)_Space_69.76_MB_(29.04%)
 
-import { TreeNode } from "../../com_github_leetcode/treenode";
+import { TreeNode } from '../../com_github_leetcode/treenode'
 
 /**
  * Definition for a binary tree node.
@@ -18,41 +18,41 @@ import { TreeNode } from "../../com_github_leetcode/treenode";
  * }
  */
 class BSTIterator {
-    private node: TreeNode | null;
+    private node: TreeNode | null
 
     constructor(root: TreeNode | null) {
-        this.node = root;
+        this.node = root
     }
 
     next(): number {
-        let res = -1;
+        let res = -1
         while (this.node !== null) {
             if (this.node.left !== null) {
-                let rightMost = this.node.left;
+                let rightMost = this.node.left
                 while (rightMost.right !== null && rightMost.right !== this.node) {
-                    rightMost = rightMost.right;
+                    rightMost = rightMost.right
                 }
 
                 if (rightMost.right === null) {
-                    rightMost.right = this.node;
-                    this.node = this.node.left;
+                    rightMost.right = this.node
+                    this.node = this.node.left
                 } else {
-                    rightMost.right = null;
-                    res = this.node.val;
-                    this.node = this.node.right;
-                    return res;
+                    rightMost.right = null
+                    res = this.node.val
+                    this.node = this.node.right
+                    return res
                 }
             } else {
-                res = this.node.val;
-                this.node = this.node.right;
-                return res;
+                res = this.node.val
+                this.node = this.node.right
+                return res
             }
         }
-        return res;
+        return res
     }
 
     hasNext(): boolean {
-        return this.node !== null;
+        return this.node !== null
     }
 }
 
